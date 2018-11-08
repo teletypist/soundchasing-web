1a45cdbcb73f3ff540c4dc8472b3421c74aa43bc
Frederick Rainbird <frainbird@gmail.com>
2018-11-08 18:19:24 +1100

    Removed google fonts fro typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -4,23 +4,6 @@ const typography = new Typography({
   baseFontSize: '16px',
   baseLineHeight: 1.5,
   scaleRatio: 2.0,
-  googleFonts: [
-      {
-        name: 'Rubik',
-        styles: [
-          '300',
-        ],
-      },
-      {
-        name: 'Lato',
-        styles: [
-          '300',
-          '300i',
-          '400',
-          '400i',
-        ],
-      },
-    ],
   headerFontFamily: ['Rubik', 'sans-serif'],
   bodyFontFamily: ['Lato', 'sans-serif'],
   bodyWeight: 300,
